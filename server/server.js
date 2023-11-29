d26bdb40be3707f0898bd7ac5ad1f481ba7777fb
luca <canerossofuoco@gmail.com>
2023-11-29 10:06:01 +0100

    modified server, .gitignore and /request/products

--- server/server.js
+++ server/server.js
@@ -1,9 +1,18 @@
-const DBMS = require('./database.js');
+const mysql = require("mysql");
 const express = require("express");
 const server = express();
-
-//creation of the database
-DBMS.start();
+const connection = mysql.createConnection({
+	host: 'localhost',
+	user: 'root',
+	password: '',
+});
+connection.connect(function(err) {
+	if (err) throw err;
+	console.log("Connected!");
+	connection.changeUser({ database: "mensapp" }, () => {
+		if (err) throw new Error(err);
+	});
+  });
 
 //deploy react
 server.use(express.static("../client/build")); //questa stringa va sostituita con "../client/build" una volta buildato il progetto  
@@ -13,9 +22,17 @@ server.get("/", (req, res) => {
 	res.sendFile(path.resolve("../client/build/index.html")); //"../client/build/index.html"
 });
 
+server.post("/request/products", (req,res) => {
+	console.log("sono dentro");
+	connection.query("SELECT * FROM prodotti", function (err, result, fields) {
+		if (err) throw err;
+		res.header("Access-Control-Allow-Origin", "*");
+		res.send(result);
+		res.end();
+	});
+});
 
-
-const port = 3000;
+const port = 6969;
 server.listen(port, () => {
 	console.log("http://localhost:" + port);
 });
