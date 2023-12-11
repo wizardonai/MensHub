db4f7235fe9f0d47a8749a05b3254e3e0625ecb4
wizardonai <matteo.donai05@gmail.com>
2023-12-11 12:24:10 +0100

    login e register server

--- server/server.js
+++ server/server.js
@@ -1,14 +1,15 @@
 const mysql = require("mysql");
 const express = require("express");
-const server = express();
-const cors = require('cors');
-const bodyParser = require('body-parser')
-
+const server = express(); //probabilmente da cambiare con express.Router();
+const emailValidator = require("deep-email-validator");
+const cors = require("cors");
+const bodyParser = require("body-parser");
 
 server.use(cors());
-server.use(bodyParser.json())
-server.use(bodyParser.urlencoded({ extended: false }))
+server.use(bodyParser.json());
+server.use(bodyParser.urlencoded({ extended: false }));
 
+//creo connessione con il database
 const connection = mysql.createConnection({
 	host: "localhost",
 	user: "root",
@@ -33,12 +34,15 @@ server.get("/", (req, res) => {
 server.post("/request/products", (req, res) => {
 	let data = req.body;
 
-	connection.query("SELECT * FROM prodotti where id_mensa=" + data.idm, (err, result) => {
-		if (err) throw new Error(err);
-		res.header("Access-Control-Allow-Origin", "*");
-		res.send(result);
-		res.end();
-	});
+	connection.query(
+		"SELECT * FROM prodotti where id_mensa=" + data.idm,
+		(err, result) => {
+			if (err) throw new Error(err);
+			res.header("Access-Control-Allow-Origin", "*");
+			res.send(result);
+			res.end();
+		}
+	);
 });
 
 server.post("/send/cart", (req, res) => {
@@ -50,14 +54,12 @@ server.post("/send/cart", (req, res) => {
 	let query = `INSERT INTO ordini (id_mensa, str_prod, quantita) VALUES(${data[0].id_mensa},"`;
 	data.forEach((item, index) => {
 		query += `${item.id}`;
-		if (index !== data.length - 1)
-			query += ",";
+		if (index !== data.length - 1) query += ",";
 	});
-	query+=`","`;
+	query += `","`;
 	data.forEach((item, index) => {
 		query += `${item.quantita}`;
-		if (index !== data.length - 1)
-			query += ",";
+		if (index !== data.length - 1) query += ",";
 	});
 	query += `");`;
 	console.log(query);
@@ -67,9 +69,6 @@ server.post("/send/cart", (req, res) => {
 		res.send("Ordine aggiunto");
 		res.end();
 	});
-
-
-
 	// connection.query("SELECT * FROM prodotti where id_mensa="+data.idm, (err, result) => {
 	// 	if (err) throw new Error(err);
 	// 	console.log(result);
@@ -79,6 +78,75 @@ server.post("/send/cart", (req, res) => {
 	// });
 });
 
+server.post("/register/user", async function (req, res) {
+	console.log("-----------------");
+	console.log("registrazione utente");
+	//forse si può provare anche con questa sintassi : const {email, password} = req.body; e poi usare email e password come variabili
+	let nome = req.body.nome;
+	let cognome = req.body.cognome;
+	let email = req.body.email;
+	let password = req.body.password;
+	let confirm_password = req.body.confirm_password;
+	if (!email || !password) {
+		return res.status(400).send({
+			message: "email o password mancante.",
+		});
+	}
+	if (password !== confirm_password) {
+		return res.status(400).send({
+			message: "le password non combaciano.",
+		});
+	}
+	const { valid, reason, validators } = await emailValidator.validate(email);
+
+	if (valid) {
+		//inserisci dati nel database
+		query = `INSERT INTO utenti (nome,cognome,email,password) VALUES('${nome}','${cognome}','${email}','${password}');`;
+		console.log(query);
+		connection.query(query, (err, result) => {
+			if (err) throw new Error(err);
+			res.header("Access-Control-Allow-Origin", "*");
+			res.send("Utente aggiunto");
+			res.end();
+		});
+	} else {
+		console.log("EMAIL NON VALIDA");
+		return res.status(400).send({
+			message: "Please provide a valid email address.",
+			reason: validators[reason].reason,
+		});
+	}
+});
+
+server.post("/login/user", async function (req, res) {
+	console.log("-----------------");
+	console.log("login");
+	let email = req.body.email;
+	let password = req.body.password;
+
+	const { valid, reason, validators } = await emailValidator.validate(email);
+	if (valid) {
+		query = `SELECT * FROM utenti WHERE email="${email}" AND password="${password}";`;
+		connection.query(query, (err, result) => {
+			if (err) throw new Error(err);
+			res.header("Access-Control-Allow-Origin", "*");
+			if (result.length === 1) {
+				//bisogna creare tutti i dati di sessione per aprire la sessione con l'utente appunto
+				res.send("Login effettuato");
+				res.end();
+			} else {
+				res.send("Utente non trovato");
+				res.end();
+			}
+		});
+	} else {
+		console.log("EMAIL NON VALIDA");
+		return res.status(400).send({
+			message: "Please provide a valid email address.",
+			reason: validators[reason].reason,
+		});
+	}
+});
 
 const port = 6969;
 server.listen(port, () => {
