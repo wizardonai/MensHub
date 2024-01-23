6c44ae1fd5e602dd3796f5999c85c4fbc67a146b
Lapo <simolapomarda@gmail.com>
2024-01-23 23:20:55 +0100

    Richiesta prodotti determinata mensa

--- server/server.js
+++ server/server.js
@@ -283,6 +283,29 @@ server.post("/request/orders", (req, res) => {
 	});
 });
 
+server.post("/producer/get/products", upload.single('image'), (req, res) => {
+	const {id_utente} = req.body;
+
+	let query = `SELECT id_mensa FROM utenti WHERE id="${id_utente};"`;
+
+		connection.query(query, (err, result) => {
+			if (err) throw new Error(err);
+
+			const id_mensa = result[0].id_mensa;
+
+			connection.query(
+				"SELECT * FROM prodotti where id_mensa=" + id_mensa,
+				(err, result) => {
+					if (err) throw new Error(err);
+					res.header("Access-Control-Allow-Origin", "*");
+					res.send(result);
+					res.end();
+				}
+			);
+		});
+
+});
+
 server.post("/producer/add/products", upload.single('image'), (req, res) => {
 	const { id_utente, nome, descrizione, allergeni, prezzo, categoria, disponibile } = req.body;
 	let id_mensa = "";
